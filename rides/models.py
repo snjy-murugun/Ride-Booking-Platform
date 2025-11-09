7f9c4a3afe7e863b4e8e6a4ede4d1a7c8467fc98
Sanjay Murugan <sanjaymurugun@gmail.com>
2025-11-09 13:00:22 +0530

    Implemented and Tested Full Trip Lifecycle : requested -> accepted -> started -> completed -> reviewed

--- rides/models.py
+++ rides/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from users.models import user
 
 class Vehicle(models.Model):                                     
     driver = models.ForeignKey(                                  
--- rides/views.py
+++ rides/views.py
@@ -3,6 +3,9 @@ from .models import Vehicle, Ride, TripLog, Payment, Review
 from .serializers import VehicleSerializer, RideSerializer, TripLogSerializer, PaymentSerializer, ReviewSerializer
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsRiderOrAdmin, IsDriverOrAdmin, IsAdmin
+from rest_framework.decorators import action
+from rest_framework import status 
+from rest_framework.response import Response
 
 
 class VehicleViewSet(viewsets.ModelViewSet):
@@ -24,6 +27,35 @@ class RideViewSet(viewsets.ModelViewSet):
         elif self.action in ['update', 'partial_update']:
             return [IsDriverOrAdmin()]
         return [IsAuthenticated()]
+    
+    def perform_create(self, serializer):
+        
+        review = serializer.save(reviewer=self.request.user)
+
+        ride = review.ride
+        if ride.status != 'reviewed':
+            ride.status = 'reviewed'
+            ride.save()
+    
+    # custom mini api named status for triplife cycle
+    @action(detail=True, methods=['patch'], url_path='status')
+    def change_status(self, request, pk=None):
+        ride = self.get_object()
+        new_status = request.data.get('status')
+
+        allowed_status = ['accepted', 'started', 'completed', 'cancelled']
+        if new_status not in allowed_status:
+            return Response({"error": "Invalid status."}, status=status.HTTP_400_BAD_REQUEST)
+
+        ride.status = new_status
+        ride.save()
+
+        return Response({
+            "message": f"Ride status updated to {new_status}.",
+            "ride_id": ride.id,
+            "status": ride.status
+        }, status=status.HTTP_200_OK)
+
 
 class TripLogViewSet(viewsets.ModelViewSet):
     queryset = TripLog.objects.all()
